5434defc230196de486c58a5127ecd30c824d352
Zhigang Song <songzhigang.szg@bytedance.com>
2025-08-21 00:34:01 +0800

    code format

--- include/sys/times.h
+++ include/sys/times.h
@@ -4,12 +4,12 @@
 #include <sys/types.h>
 
 struct tms {
-	time_t tms_utime;
-	time_t tms_stime;
-	time_t tms_cutime;
-	time_t tms_cstime;
+    time_t tms_utime;
+    time_t tms_stime;
+    time_t tms_cutime;
+    time_t tms_cstime;
 };
 
-extern time_t times(struct tms * tp);
+extern time_t times(struct tms *tp);
 
 #endif
--- include/utime.h
+++ include/utime.h
@@ -1,11 +1,11 @@
 #ifndef _UTIME_H
 #define _UTIME_H
 
-#include <sys/types.h>	/* I know - shouldn't do this, but .. */
+#include <sys/types.h> /* I know - shouldn't do this, but .. */
 
 struct utimbuf {
-	time_t actime;
-	time_t modtime;
+    time_t actime;
+    time_t modtime;
 };
 
 extern int utime(const char *filename, struct utimbuf *times);
