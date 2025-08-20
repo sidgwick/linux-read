30a858d85d7c0efac7253818ddf1e69ed2da2cb2
Zhigang Song <songzhigang.szg@bytedance.com>
2025-08-21 00:36:42 +0800

    注解 include

--- include/errno.h
+++ include/errno.h
@@ -14,51 +14,55 @@
  * NOTE! Remember to change strerror() if you change this file!
  */
 
+/* 系统调用以及很多库函数返回一个特殊的值以表示操作失败或出错, 这个值通常选择 -1
+ * 或者 其他一些特定的值来表示. 但是这个返回值仅说明错误发生了.
+ * 如果需要知道出错的类型, 就需要查看表示系统出错号的变量 errno, 该变量即在
+ * errno.h 文件中声明 在程序开始执行时该变量值被初始化为 0 */
 extern int errno;
 
-#define ERROR 99
-#define EPERM 1
-#define ENOENT 2
-#define ESRCH 3
-#define EINTR 4
-#define EIO 5
-#define ENXIO 6
-#define E2BIG 7
-#define ENOEXEC 8
-#define EBADF 9
-#define ECHILD 10
-#define EAGAIN 11
-#define ENOMEM 12
-#define EACCES 13
-#define EFAULT 14
-#define ENOTBLK 15
-#define EBUSY 16
-#define EEXIST 17
-#define EXDEV 18
-#define ENODEV 19
-#define ENOTDIR 20
-#define EISDIR 21
-#define EINVAL 22
-#define ENFILE 23
-#define EMFILE 24
-#define ENOTTY 25
-#define ETXTBSY 26
-#define EFBIG 27
-#define ENOSPC 28
-#define ESPIPE 29
-#define EROFS 30
-#define EMLINK 31
-#define EPIPE 32
-#define EDOM 33
-#define ERANGE 34
-#define EDEADLK 35
-#define ENAMETOOLONG 36
-#define ENOLCK 37
-#define ENOSYS 38
-#define ENOTEMPTY 39
+#define ERROR 99        // 一般错误
+#define EPERM 1         // 无权限
+#define ENOENT 2        // 文件或者目录不存在
+#define ESRCH 3         // 指定的进程不存在
+#define EINTR 4         // 中断的系统调用
+#define EIO 5           // IO 错误
+#define ENXIO 6         // 指定设备/地址不存在
+#define E2BIG 7         // 参数列表太长
+#define ENOEXEC 8       // 执行程序格式错误
+#define EBADF 9         // 文件句柄(描述符)错误
+#define ECHILD 10       // 子进程不存在
+#define EAGAIN 11       // 资源暂时不可用
+#define ENOMEM 12       // 内存不足
+#define EACCES 13       // 没有许可权限
+#define EFAULT 14       // 地址错误
+#define ENOTBLK 15      // 不是块设备文件
+#define EBUSY 16        // 资源忙
+#define EEXIST 17       // 文件已经存在
+#define EXDEV 18        // 非法连接
+#define ENODEV 19       // 设备不存在
+#define ENOTDIR 20      // 不是目录文件
+#define EISDIR 21       // 是目录文件
+#define EINVAL 22       // 参数无效
+#define ENFILE 23       // 系统打开文件数量过多
+#define EMFILE 24       // 打开文件数量过多
+#define ENOTTY 25       // 不恰当的 IO 控制操作(没有 tty 终端)
+#define ETXTBSY 26      // 不再使用??
+#define EFBIG 27        // 文件太大
+#define ENOSPC 28       // 设备满(无空间)
+#define ESPIPE 29       // 无效的文件指针重定位
+#define EROFS 30        // 文件系统只读
+#define EMLINK 31       // 链接太多
+#define EPIPE 32        // 管道错误
+#define EDOM 33         // 域(domain)错误
+#define ERANGE 34       // 结果太大
+#define EDEADLK 35      // 避免资源死锁
+#define ENAMETOOLONG 36 // 文件名太长
+#define ENOLCK 37       // 没有锁定可用
+#define ENOSYS 38       // 功能还没有实现
+#define ENOTEMPTY 39    // 目录不为空
 
 /* Should never be seen by user programs */
-#define ERESTARTSYS 512
-#define ERESTARTNOINTR 513
+#define ERESTARTSYS 512    // 重新执行系统调用
+#define ERESTARTNOINTR 513 // 重新执行系统调用, 无中断
 
 #endif
